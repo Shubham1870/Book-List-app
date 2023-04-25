8e1b6c5227d62f39a6e31740b401739c94615050
Shuubham <107632088+Shubham1870@users.noreply.github.com>
2023-04-25 11:48:45 +0530

    Update signin.js

--- src/Components/Signin/signin.js
+++ src/Components/Signin/signin.js
@@ -1,70 +1,91 @@
 import React from "react"
 import "../Signin/signin.css"
-import {Link, useNavigate}  from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { useState } from "react"
-const Signin=()=>{
-    const [data,setdata]=useState({
-        email:"",
-        password:""
+
+const Signin = () => {
+  const [data, setdata] = useState({
+    email: "",
+    password: "",
+  })
+  const navigate = useNavigate()
+  const [err, seterr] = useState("")
+
+  const handlechange = (e) => {
+    const { name, value } = e.target
+    setdata({ ...data, [name]: value })
+  }
+
+  const handlesubmit = async (e) => {
+    e.preventDefault()
+
+    await fetch("https://book-backend2.onrender.com/signin", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
     })
-    const navigate=useNavigate()
-    const [err,seterr]=useState("")
-    const handlechange=(e)=>{
-     const {name,value}=e.target
-     setdata({...data,[name]:value})
-    }
-    const handlesubmit=async (e)=>{
-        e.preventDefault()
+      .then((res) => {
+        return res.json()
+      })
+      .then((data) => {
+        if (data.message === "Wrong password") {
+          seterr(data.message)
+          return
+        }
+        seterr(data.message)
+        if (data.message === "Login Succesfull") {
+          navigate("/homepage")
+        }
 
-        await fetch("https://book-backend2.onrender.com/signin",{
-            method:"POST",
-            headers:{
-                Accept:"application/json",
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify(data)
-        }).then(res=>{
-            return res.json()
-        }).then(data=>{
-            if(data.message==="Wrong password"){
-                seterr(data.message)
-                return
-            }
-            seterr(data.message)
-             if(data.message==="Login Succesfull"){	
-                navigate("/homepage")	
-            }
-           
-            localStorage.setItem("token",data.token)
-            localStorage.setItem("id",data.id)
-             console.log(data,"data")
-            }
+        localStorage.setItem("token", data.token)
+        localStorage.setItem("id", data.id)
+        console.log(data, "data")
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
 
-        }).catch(err=>{
-            console.log(err)
-        })
-    }
-    return (
-        <>
-<div id="main-container">
-    <div id="inside-container">
-<h1>Member Login</h1>
-<form onSubmit={handlesubmit}>
-    <label>Email:</label>
-    <input type="email" name="email" id="first-input" required onChange={(e)=>{handlechange(e)}} />
-    <br/>
-    <label>Password:</label>
-    <input type="password" name="password" onChange={(e)=>{handlechange(e)}}/>
-    <br/>
-    <button type="submit">Submit</button><br/>
-    <button><Link to="/signup">SignUp</Link></button>
-    <div>{err}</div>
-
-
-</form>
-    </div>
-</div>
-        </>
-    )
+  return (
+    <>
+      <div id="main-container">
+        <div id="inside-container">
+          <h1>Member Login</h1>
+          <form onSubmit={handlesubmit}>
+            <label>Email:</label>
+            <input
+              type="email"
+              name="email"
+              id="first-input"
+              required
+              onChange={(e) => {
+                handlechange(e)
+              }}
+            />
+            <br />
+            <label>Password:</label>
+            <input
+              type="password"
+              name="password"
+              onChange={(e) => {
+                handlechange(e)
+              }}
+            />
+            <br />
+            <button type="submit">Submit</button>
+            <br />
+            <button>
+              <Link to="/signup">SignUp</Link>
+            </button>
+            <div>{err}</div>
+          </form>
+        </div>
+      </div>
+    </>
+  )
 }
+
 export default Signin
