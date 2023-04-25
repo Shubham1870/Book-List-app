1e6d162e278254a49d7a3078726f5c731215bb9b
Shuubham <107632088+Shubham1870@users.noreply.github.com>
2023-04-25 12:51:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const app=express()
 const cors=require("cors")
-const PORT=8000|| "https://book-backend2.onrender.com"
+const PORT=process.env.PORT || 8000
 const dotenv=require("dotenv")
 const connection=require("./db/connection")()
 const Userroute=require("./routes/user")
@@ -11,9 +11,6 @@ dotenv.config()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
-
-
-
 
 app.use("/",Userroute)
 app.use("/",Bookroute)
